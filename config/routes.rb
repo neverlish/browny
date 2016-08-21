57358534bc253f200e0cb619d9c34d8ecc91702d
Namwon Heo <namwonheo89@gmail.com>
2016-08-21 20:33:43 +0900

    for checking

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
   get '/posts' => 'posts#index'
   get '/spots/category/:category' => 'spots#category'
-  
+  get '/welcome/index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   root 'places#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
