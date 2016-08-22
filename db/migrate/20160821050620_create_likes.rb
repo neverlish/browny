09bee3220339a32b89cda55b56c7b6ebba78c106
YeongBeomKim <kyb5493@gmail.com>
2016-08-22 11:15:18 +0900

    모델정리 및 place지도기능추가

--- db/migrate/20160821050620_create_likes.rb
+++ db/migrate/20160821050620_create_likes.rb
@@ -3,7 +3,7 @@ class CreateLikes < ActiveRecord::Migration[5.0]
     create_table :likes do |t|
       t.references :user, foreign_key: true
       t.references :spot, foreign_key: true
-
+      t.references :post, foreign_key: true
       t.timestamps
     end
   end
