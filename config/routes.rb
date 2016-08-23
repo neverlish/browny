059875ca4f1bc7b9639dc2a9ff8b9e6c5bc1e2e7
Namwon Heo <namwonheo89@gmail.com>
2016-08-23 22:04:46 +0900

    navigation link ing

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   get '/spots/category/:category' => 'spots#category'
   get '/welcome/index'
   
-  root 'welcome#main'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
